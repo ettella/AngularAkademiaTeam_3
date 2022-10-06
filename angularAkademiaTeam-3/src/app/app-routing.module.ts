96ddd32055a6abc6b87cde1a50c081b3618738f5
ettella <ettella@hotmail.com>
2022-10-06 07:40:09 +0200

    1.task - done

--- angularAkademiaTeam-3/src/app/app-routing.module.ts
+++ angularAkademiaTeam-3/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import { FourthExerciseComponent } from './components/fourth-exercise/fourth-exercise.component';
+import { FirstExerciseComponent } from './components/first-exercise/first-exercise.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SecondExerciseComponent } from './components/second-exercise/second-exercise.component';
+import { ThirdExerciseComponent } from './components/third-exercise/third-exercise.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'task-1', component: FirstExerciseComponent},
+  {path: 'task-2', component: SecondExerciseComponent},
+  {path: 'task-3', component: ThirdExerciseComponent},
+  {path: 'task-4', component: FourthExerciseComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angularAkademiaTeam-3/src/app/app.module.ts
+++ angularAkademiaTeam-3/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstExerciseComponent } from './components/first-exercise/first-exercise.component';
+import { SecondExerciseComponent } from './components/second-exercise/second-exercise.component';
+import { FourthExerciseComponent } from './components/fourth-exercise/fourth-exercise.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ThirdExerciseComponent } from './components/third-exercise/third-exercise.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstExerciseComponent,
+    SecondExerciseComponent,
+    FourthExerciseComponent,
+    NavbarComponent,
+    ThirdExerciseComponent
   ],
   imports: [
     BrowserModule,
