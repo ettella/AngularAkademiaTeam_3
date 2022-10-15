5b6fb37a1bce1864e5ce3593a5ef9e554ceab776
Szász <Fanni.Szasz@t-systems.com>
2022-10-15 16:13:05 +0200

    fix login bug

--- src/app/components/week_1/auth/auth.component.ts
+++ src/app/components/week_1/auth/auth.component.ts
@@ -1,57 +1,60 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../../service/auth.service';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.scss']
+  styleUrls: ['./auth.component.scss'],
 })
 export class AuthComponent implements OnInit {
-
   signInForm: FormGroup;
-
-  // email = new FormControl('', [Validators.required, Validators.email]);
-  // password = new FormControl('', [Validators.required]);
   hide = true;
 
-  constructor(private authService: AuthService, private fb: FormBuilder, private router: Router) {
+  constructor(
+    private authService: AuthService,
+    public fb: FormBuilder,
+    private router: Router
+  ) {
     this.signInForm = fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required],
     });
-   }
-
-  get email() : FormControl{
-    return this.email.get('email') as FormControl
   }
 
-  get password() : FormControl{
-    return this.signInForm.get('password') as FormControl
+   get email(): FormControl {
+    return this.signInForm.get('email') as FormControl;
   }
 
-  ngOnInit(): void {
+   get password(): FormControl {
+    return this.signInForm.get('password') as FormControl;
   }
 
-  onSubmit() {
-    this.authService.signin(this.email.value["email"], this.password.value["password"]);
+  ngOnInit(): void {}
+
+  protected onSubmit(): void {
+    this.authService.signin(this.email.value, this.password.value);
     this.router.navigate(['home']);
   }
-  
 
-  // getEmailErrorMessage() {
-  //   if (this.email.hasError('required')) {
-  //     return 'You must enter a value';
-  //   }
+  getEmailErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter your email!';
+    }
 
-  //   return this.email.hasError('email') ? 'Not a valid email' : '';
-  // }
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+  getPasswordErrorMessage() {
+    if (this.password.hasError('required')) {
+      return 'You must enter your password!';
+    }
 
-  // getPasswordErrorMessage() {
-  //   if (this.password.hasError('required')) {
-  //     return 'You must enter a value';
-  //   }
-  //   return this.password.hasError('password') ? 'Not a valid password' : '';
-  // }
+    return this.password.hasError('password') ? 'Not a valid password' : '';
+  }
 }
