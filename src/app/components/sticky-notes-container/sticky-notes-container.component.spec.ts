9c2a847836e232caa7048958e1c8c7f39b185a9a
funkster <fanni.szasz96@gmail.com>
2022-10-18 20:50:23 +0200

    add tests for notes

--- src/app/components/sticky-notes-container/sticky-notes-container.component.spec.ts
+++ src/app/components/sticky-notes-container/sticky-notes-container.component.spec.ts
@@ -41,4 +41,80 @@ describe('StickyNotesContainerComponent', () => {
 
   })
 
+  it('should delete the selected note', () => {
+    expect(el.querySelectorAll('.notes').length).toBe(4);
+    const deleteIcon = el.querySelector('#delete')
+    deleteIcon.dispatchEvent(new Event('click'))
+    fixture.detectChanges();
+    expect(el.querySelectorAll('.notes').length).toBe(3);
+  })
+
+  it('should delete all notes on Delete all CTA', () => {
+    expect(el.querySelectorAll('.notes').length).toBe(4);
+    const deleteIcon = el.querySelector('#deleteall')
+    deleteIcon.dispatchEvent(new Event('click'))
+    fixture.detectChanges();
+    expect(el.querySelectorAll('.notes').length).toBe(0);
+  })
+
+  it('should edit the sticky note', () => {
+
+  })
+
+  it('should be align items to the center horizontally', () => {
+    const noteContainer: HTMLElement = el.querySelector('.notes-container')
+    el.querySelector('#horizontal-center').dispatchEvent(new Event('click'))
+    fixture.detectChanges()
+    expect(el.querySelector('.notes-container').getAttribute("style").endsWith("center;")).toBeTrue()
+  })
+
+  it('should be align items to the start horizontally', () => {
+    const noteContainer: HTMLElement = el.querySelector('.notes-container')
+    el.querySelector('#horizontal-start').dispatchEvent(new Event('click'))
+    fixture.detectChanges()
+    expect(el.querySelector('.notes-container').getAttribute("style").endsWith("start;")).toBeTrue()
+  })
+
+  it('should be align items to the end horizontally', () => {
+    const noteContainer: HTMLElement = el.querySelector('.notes-container')
+    el.querySelector('#horizontal-end').dispatchEvent(new Event('click'))
+    fixture.detectChanges()
+    expect(el.querySelector('.notes-container').getAttribute("style").endsWith("end;")).toBeTrue()
+  })
+
+  it('should be align items to the center horizontally', () => {
+    const noteContainer: HTMLElement = el.querySelector('.notes-container')
+    el.querySelector('#horizontal-space-between').dispatchEvent(new Event('click'))
+    fixture.detectChanges()
+    expect(el.querySelector('.notes-container').getAttribute("style").endsWith("space-between;")).toBeTrue()
+  })
+
+  it('should be align items to space-between vertically', () => {
+    const noteContainer: HTMLElement = el.querySelector('.notes-container')
+    el.querySelector('#vertical-center').dispatchEvent(new Event('click'))
+    fixture.detectChanges()
+    expect(el.querySelector('.notes-container').getAttribute("style").startsWith("place-content: center")).toBeTrue()
+  })
+
+  it('should be align items to the start vertically', () => {
+    const noteContainer: HTMLElement = el.querySelector('.notes-container')
+    el.querySelector('#vertical-start').dispatchEvent(new Event('click'))
+    fixture.detectChanges()
+    expect(el.querySelector('.notes-container').getAttribute("style").startsWith("place-content: start")).toBeTrue()
+  })
+
+  it('should be align items to the end vertically', () => {
+    const noteContainer: HTMLElement = el.querySelector('.notes-container')
+    el.querySelector('#vertical-end').dispatchEvent(new Event('click'))
+    fixture.detectChanges()
+    expect(el.querySelector('.notes-container').getAttribute("style").startsWith("place-content: end")).toBeTrue()
+  })
+
+  it('should be align items to space-between vertically', () => {
+    const noteContainer: HTMLElement = el.querySelector('.notes-container')
+    el.querySelector('#vertical-space-between').dispatchEvent(new Event('click'))
+    fixture.detectChanges()
+    expect(el.querySelector('.notes-container').getAttribute("style").startsWith("place-content: space-between")).toBeTrue()
+  })
+
 });
