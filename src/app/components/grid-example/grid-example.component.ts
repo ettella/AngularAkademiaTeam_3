60354e8532945c090290691431359b4a13b2df20
ettella <ettella@hotmail.com>
2022-10-18 09:28:38 +0200

    Add images to mat-grid system

--- src/app/components/grid-example/grid-example.component.ts
+++ src/app/components/grid-example/grid-example.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 export interface Tile {
-  color: string;
   cols: number;
   rows: number;
-  text: string;
 }
 @Component({
   selector: 'app-grid-example',
@@ -15,25 +13,25 @@ export interface Tile {
 export class GridExampleComponent implements OnInit {
 
   tiles1: Tile[] = [
-    {text: 'One', cols: 1, rows: 2, color: 'lightblue'},
-    {text: 'Two', cols: 2, rows: 2, color: 'lightblue'},
-    {text: 'Three', cols: 3, rows: 1, color: 'lightblue'}
+    {cols: 1, rows: 2 },
+    {cols: 2, rows: 2 },
+    {cols: 3, rows: 1 },
   ];
 
   tiles2: Tile[] = [
-    {text: 'One', cols: 2, rows: 3, color: '#DDBDF1'},
-    {text: 'Two', cols: 1, rows: 1, color: '#DDBDF1'},
-    {text: 'Three', cols: 1, rows: 1, color: '#DDBDF1'},
-    {text: 'Four', cols: 1, rows: 1, color: '#DDBDF1'},
+    {cols: 2, rows: 3 },
+    {cols: 1, rows: 1 },
+    {cols: 1, rows: 1 },
+    {cols: 1, rows: 1 },
   ];
 
   tiles3: Tile[] = [
-    {text: 'One', cols: 3, rows: 4, color: 'lightpink'},
-    {text: 'Two', cols: 3, rows: 4, color: 'lightpink'},
-    {text: 'Three', cols: 2, rows: 2, color: 'lightpink'},
-    {text: 'Four', cols: 2, rows: 1, color: 'lightpink'},
-    {text: 'Six', cols: 2, rows: 2, color: 'lightpink'},
-    {text: 'Five', cols: 2, rows: 1, color: 'lightpink'},
+    {cols: 3, rows: 4 },
+    {cols: 3, rows: 4 },
+    {cols: 2, rows: 2 },
+    {cols: 2, rows: 1 },
+    {cols: 2, rows: 2 },
+    {cols: 2, rows: 1 },
   ];
 
   constructor() { }
